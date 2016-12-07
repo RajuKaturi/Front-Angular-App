d0ebc6c6c03f21b6ce8f9e0430e37284abb776b7
Raju K <raju@olivetech.net>
2016-12-07 20:35:56 +0530

    IG-56: Create Xml file in the server side from Son Object for Recurly and ACH.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var cors = require('cors');
 var request = require('request');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
@@ -29,39 +29,52 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+// app.use('/users', users);
 app.use('/api',api);
 
-app.get('/recurly', function (req, res) {
+app.post('/recurly', function (req, res) {
+  //console.log('this is recurly...');
+//  console.log(req.body);
+  var paymentData = req.body;
+  var amount = 6000;
+  //console.log(paymentData);
     var checked  = true;
-    if(checked) {
+    if(paymentData.monthlyGiving) {
         console.log('in if');
-        var sub_body = "<subscription href='https://kids-discover-test.recurly.com/v2/subscriptions' type='credit_card'><plan_code>ifgmonthlysb</plan_code><unit_amount_in_cents type='integer'>6000</unit_amount_in_cents><currency>USD</currency><account><account_code>gowtham@gmail.com</account_code><first_name>gowtham</first_name><last_name>kumar</last_name><email>gowtham@gmail.com</email><billing_info type='credit_card'><first_name>gowtham</first_name><last_name>kumar</last_name><address1>123 Main St.</address1><address2 nil='nil'/><city>San Francisco</city><state>CO</state><zip>80912</zip><country>US</country><phone nil='nil'/><vat_number nil='nil'/><year type='integer'>2018</year><month type='integer'>11</month><number>4111-1111-1111-1111</number></billing_info></account></subscription>";
+        var sub_body = "<subscription href='https://kids-discover-test.recurly.com/v2/subscriptions' type='credit_card'><plan_code>ifgmonthlysb</plan_code><unit_amount_in_cents type='integer'>"+ paymentData.amount+"</unit_amount_in_cents><currency>USD</currency><account><account_code>"+ paymentData.emailId+"</account_code><first_name>"+ paymentData.firstName+"</first_name><last_name>"+ paymentData.lastName+"</last_name><email>"+ paymentData.emailId+"</email><billing_info type='credit_card'><first_name>"+ paymentData.firstName+"</first_name><last_name>"+ paymentData.lastName+"</last_name><address1>"+ paymentData.lastName+"</address1><address2 nil='nil'/><city>"+ paymentData.city+"</city><state>CO</state><zip>"+ paymentData.zipCode+"</zip><country>US</country><phone nil='nil'/><vat_number nil='nil'/><year type='integer'>"+paymentData.CCYear+"</year><month type='integer'>"+paymentData.CCMonth+"</month><number>"+ paymentData.CCNumber+"</number></billing_info></account></subscription>";
         var sub_url = "https://kids-discover-test.recurly.com/v2/subscriptions";
         var sub_headers = {
             'Accept': 'application/xml',
             'Authorization': 'Basic N2E0YTI3OTY3ZDY3NGQ2ODg1OTIzOTMzYmNlZjEyZmQ='
         } ;
         request.post({ url: sub_url,body: sub_body,headers: sub_headers}, function(error, response, body) {
-
-            if (!error && response.statusCode == 200) {
-                var info = JSON.parse(body);
-                console.log(info);
-                // return res.send(info);
-            } else {
-                return res.send(response.body);
-            }
+          console.log("response.statusCode");
+          console.log(response.statusMessage);
+          //res.send('This is King.......');
+            // if (!error && response.statusCode == 200) {
+            //   console.log('true.....');
+            //     var info = JSON.parse(body);
+            //     console.log(info);
+            //     // return res.send(info);
+            //     return res.send('This is true.......');
+            // } else {
+            //   console.log('false....');
+            //     return res.send('This is failure....');
+            // }
+          return res.send(response.statusMessage);
         });
     } else {
         console.log('in else');
-        var body = "<transaction href='https://kids-discover-test.recurly.com/v2/transactions'><account href='https://kids-discover-test.recurly.com/v2/accounts/vara.rasuri021@gmail.com'/><amount_in_cents type='integer'>1000</amount_in_cents><currency>USD</currency><payment_method>credit_card</payment_method><account><account_code>vara.rasuri021@gmail.com</account_code><first_name>vAraprasdd</first_name><last_name>rasuri</last_name><email>vara.rasuri021@gmail.com</email><billing_info type='credit_card'><first_name>Varaprasad</first_name><last_name>Rasuri</last_name><address1>123 Main St.</address1><address2 nil='nil'/><city>San Francisco</city><state>CO</state><zip>80912</zip><country>US</country><phone nil='nil'/><vat_number nil='nil'/><year type='integer'>2018</year><month type='integer'>11</month><number>4111-1111-1111-1111</number></billing_info></account></transaction>";
+        var AccountUrl = "https://kids-discover-test.recurly.com/v2/accounts/"+paymentData.emailId;
+        console.log(AccountUrl);
+        var body = "<transaction href='https://kids-discover-test.recurly.com/v2/transactions'><account href='"+AccountUrl+"'/><amount_in_cents type='integer'>"+paymentData.amount+"</amount_in_cents><currency>USD</currency><payment_method>credit_card</payment_method><account><account_code>"+paymentData.emailId+"</account_code><first_name>"+paymentData.firstName+"</first_name><last_name>"+paymentData.lastName+"</last_name><email>"+paymentData.emailId+"</email><billing_info type='credit_card'><first_name>"+paymentData.firstName+"</first_name><last_name>"+paymentData.lastName+"</last_name><address1>"+paymentData.addressOne+"</address1><address2 nil='nil'/><city>"+paymentData.city+"</city><state>CO</state><zip>"+paymentData.zipCode+"</zip><country>US</country><phone nil='nil'/><vat_number nil='nil'/><year type='integer'>"+paymentData.CCYear+"</year><month type='integer'>"+paymentData.CCMonth+"</month><number>"+paymentData.CCNumber+"</number></billing_info></account></transaction>";
         var url = "https://kids-discover-test.recurly.com/v2/transactions";
         var headers = {
             'Accept': 'application/xml',
             'Authorization': 'Basic N2E0YTI3OTY3ZDY3NGQ2ODg1OTIzOTMzYmNlZjEyZmQ='
         } ;
         request.post({ url: url,body: body,headers: headers}, function(error, response, body) {
-
+            console.log(response.statusMessage);
             if (!error && response.statusCode == 200) {
                 var info = JSON.parse(body);
                 console.log(info);
@@ -71,11 +84,14 @@ app.get('/recurly', function (req, res) {
             }
         });
     }
+
 });
 
-app.get('/ach', function (req, res) {
+app.post('/ach', function (req, res) {
+  console.log('This is ACH Payment.....');
 
     var checked  = false;
+    var paymentDataACH = req.body;
 
     if(checked) {
         console.log('in if');
@@ -86,7 +102,7 @@ app.get('/ach', function (req, res) {
             'Authorization': 'Basic N2E0YTI3OTY3ZDY3NGQ2ODg1OTIzOTMzYmNlZjEyZmQ='
         } ;
         request.post({ url: sub_url,body: sub_body,headers: sub_headers}, function(error, response, body) {
-
+            console.log(response.statusMessage);
             if (!error && response.statusCode == 200) {
                 var info = JSON.parse(body);
                 console.log(info);
@@ -98,14 +114,16 @@ app.get('/ach', function (req, res) {
 
     } else {
         console.log('in else');
-        var body = "<transaction href='https://kids-discover-test.recurly.com/v2/transactions' type='bank_account'><account href='https://kids-discover-test.recurly.com/v2/accounts/viru1@gmail.com'/><amount_in_cents type='integer'>1000</amount_in_cents><currency>USD</currency><payment_method>ACH</payment_method><account><account_code>viru1@gmail.com</account_code><first_name>Viru</first_name><last_name>Last</last_name><email>viru1@gmail.com</email><company_name>IFG</company_name><billing_info type='bank_account'><address1>123 Main St.</address1><address2 nil='nil'/><city>San Francisco</city><state>CO</state><zip>80912</zip><country>US</country><phone nil='nil'/><vat_number nil='nil'/><name_on_account>Acme, Inc.</name_on_account><account_type>checking</account_type><account_number>111111111</account_number><company>OLIVE</company><routing_number type='integer'>123456780</routing_number></billing_info></account></transaction>";
+        var AccountUrlACH = "https://kids-discover-test.recurly.com/v2/accounts/"+paymentDataACH.emailId;
+        console.log(paymentDataACH);
+        var body = "<transaction href='https://kids-discover-test.recurly.com/v2/transactions' type='bank_account'><account href='"+AccountUrlACH+"'/><amount_in_cents type='integer'>"+paymentDataACH.amount+"</amount_in_cents><currency>USD</currency><payment_method>ACH</payment_method><account><account_code>"+paymentDataACH.emailId+"</account_code><first_name>"+paymentDataACH.firstName+"</first_name><last_name>"+paymentDataACH.lastName+"</last_name><email>"+paymentDataACH.emailId+"</email><company_name>IFG</company_name><billing_info type='bank_account'><address1>"+paymentDataACH.addressOne+"</address1><address2 nil='nil'/><city>"+paymentDataACH.city+"</city><state>CO</state><zip>"+paymentDataACH.zipCode+"</zip><country>US</country><phone nil='nil'/><vat_number nil='nil'/><name_on_account>"+paymentDataACH.accountName+"</name_on_account><account_type>checking</account_type><account_number>"+paymentDataACH.accountNumber+"</account_number><company>OLIVE</company><routing_number type='integer'>"+paymentDataACH.routingNumber+"</routing_number></billing_info></account></transaction>";
         var url = "https://kids-discover-test.recurly.com/v2/transactions";
         var headers = {
             'Accept': 'application/xml',
             'Authorization': 'Basic N2E0YTI3OTY3ZDY3NGQ2ODg1OTIzOTMzYmNlZjEyZmQ='
         } ;
         request.post({ url: url,body: body,headers: headers}, function(error, response, body) {
-
+            console.log(response.statusMessage);
             if (!error && response.statusCode == 200) {
                 var info = JSON.parse(body);
                 console.log(info);
